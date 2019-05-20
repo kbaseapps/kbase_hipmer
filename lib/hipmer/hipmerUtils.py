2ebf180b077402a1df5fa3e0b02639a397f56640
Jeff Froula <jlfroula@lbl.gov>
2019-05-20 08:58:59 -0700

    changed version to 829

--- lib/hipmer/hipmerUtils.py
+++ lib/hipmer/hipmerUtils.py
@@ -370,7 +370,7 @@ class hipmerUtils:
             f.write('echo Executing ' + hipmer_command + '\n')
             f.write('ls\n')
             f.write('echo "at $(date) on $(uname -n)"\n')
-            f.write('HIPMER_INSTALL=$(pwd)/v1.0-378*/bin\n')
+            f.write('HIPMER_INSTALL=$(pwd)/v1.0-829*/bin\n')
             f.write('${HIPMER_INSTALL}/' + hipmer_command + '\n')
             f.close()
 
