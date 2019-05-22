dbb0d1e32a89c53396dae64e5c7866c504977a65
Jeff Froula <jlfroula@lbl.gov>
2019-05-22 16:36:26 -0700

    increased number of nodes for hipmer run 20 to 25.

--- lib/hipmer/hipmerUtils.py
+++ lib/hipmer/hipmerUtils.py
@@ -7,7 +7,6 @@ import numpy as np
 from pprint import pformat
 from pprint import pprint
 import subprocess
-#import shutil
 from Bio import SeqIO
 
 from installed_clients.ReadsUtilsClient import ReadsUtils  # @IgnorePep8
@@ -330,8 +329,8 @@ class hipmerUtils:
         Generate SLURM submit script
         """
         # the formula for estimating number of nodes required
-        # nodes = 20*Gb-reads/80G
-        nodes = round( ((total_size_gigs * 20) / 80) + 1 )
+        # nodes = 25*Gb-reads/80G
+        nodes = round( ((total_size_gigs * 25) / 80) + 1 )
         if nodes < 1:
             nodes = 2 # can't be an odd number
 
